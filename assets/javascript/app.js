2a512adfd44b7db5243f3b752c3167747b4ec7b4
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2018-12-09 16:37:28 -0500

    Add OMDB integration. Add ability to favorite items.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,20 +15,26 @@ var movies = [
     "Dr. Strangelove"
 ];
 
-
 // Function for displaying movie buttons.
 function displayButtons() {
 
     var $movieButtonArea = $("#movie-buttons");
-
     $movieButtonArea.empty();
 
     $.each(movies, function(i, movie){
-
         $movieButtonArea.append($("<button type='button' class='btn btn-dark m-1'>" + movie + "</button>").attr("data-name", movie).addClass("movie"));
-
     });
 
+};
+
+function hideExtraContent() {
+    $("#omdb-display").hide();
+    $("#your-favorites").hide();
+};
+
+function showExtraContent() {
+    $("#omdb-display > img").remove();
+    $("#omdb-display").show("medium");
 };
 
 function clearGifs() {
@@ -61,12 +67,21 @@ function giphyQuery() {
 
         for (var i = 0; i < gifs.length; i++) {
 
-            var $badge = $("<a href='#' />").append($("<span class='badge badge-pill badge-danger figure-badge p-2'>+ Favorites</span>"));
+            // Hide this bit if you want the special effects (.hide()).
+            var $badge = $("<span class='badge badge-pill badge-danger figure-badge p-2'>+ Favorites</span>");
+
+            $badge.attr({
+                "data-still-small": gifs[i].images.fixed_height_small_still.url,
+                "data-animate-small": gifs[i].images.fixed_height_small.url
+            });
 
             var $img = $("<img class='figure-img img-fluid rounded' src='" + gifs[i].images.fixed_height_still.url + "'/>");
 
-            $img.attr("data-animate", gifs[i].images.fixed_height.url);
-            $img.attr("data-still", gifs[i].images.fixed_height_still.url);
+            $img.attr({
+                "data-animate": gifs[i].images.fixed_height.url,
+                "data-still": gifs[i].images.fixed_height_still.url,
+            });
+
             $img.addClass("gif-still");
 
             var $figCaption = $("<figcaption class='figure-caption text-center'>Rating: " + gifs[i].rating + "</figcaption>");
@@ -78,6 +93,42 @@ function giphyQuery() {
             $target.prepend($gif);
         }
 
+        // $target.append("<button class='btn btn-dark mb-3 mt-3' id='load-more'>Load More!</button>");
+
+        // $("#load-more").on("click", function() {
+
+        // });
+
+    });
+
+};
+
+// Query the OMDB database and add data to page.
+function queryOMDB() {
+
+    var $current = $(this).attr("data-name");
+    var baseURL = "https://www.omdbapi.com/?t=";
+    var URL = baseURL + $current + "&y=&plot=short&apikey=" + returnKey();
+    var $target = $("#omdb-display");
+
+    // Return API key (not sure if this is more secure).
+    function returnKey() {
+        var key = "trilogy";
+        return key;
+    }
+
+    // Query the OMDB API.
+    $.ajax({
+        url: URL,
+        method: "GET"
+    }).then(function(response){
+
+        $("#movie-title").text(response.Title);
+        $("#date-released").text(response.Released);
+        $("#director").text(response.Director);
+        $("#synopsis").text(response.Plot);
+        $("#omdb-display").append("<img src='" + response.Poster + "'/img>");
+        
     });
 
 };
@@ -99,22 +150,35 @@ $("#submit-movie-button").on("click", function(e) {
 });
 
 /* 
-    When a movie button is clicked, 
-    query GIPHY and OMDB database 
-    and then display corresponding set of
-    gifs and movie specs.
+    When a movie button is clicked, query GIPHY and OMDB database 
+    and then display corresponding set of gifs and movie specs.
 */
-$(document).on("click", ".movie", giphyQuery);
+$(document).on("click", ".movie", giphyQuery)
+           .on("click", ".movie", queryOMDB)
+           .on("click", ".movie", showExtraContent);
 
 // Display animated gif if hovering over still image.
 $(document).on({
     mouseenter: function() {
         $(this).attr("src", $(this).attr("data-animate"));
+        // $(".figure-badge").first().show("slow");
     },
     mouseleave: function() {
         $(this).attr("src", $(this).attr("data-still"));
+        // $(".figure-badge").first().hide("slow");
     }
 }, ".gif-still");
 
+// Add small gif to your favorites.
+$(document).on("click", ".figure-badge", function() {
+    var $smallStill = $(this).attr("data-still-small");
+    var $smallAnimate = $(this).attr("data-animate-small");
+
+    $("#your-favorites").show("medium");
+
+    $("#your-favorites").append("<img class='m-2' src='" + $(this).attr("data-animate-small") + "'/>");
+});
+
 // Display initial set of gif buttons.
 displayButtons();
+hideExtraContent();
